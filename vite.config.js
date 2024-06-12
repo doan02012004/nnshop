1739281e172c0ee3fd78f77acd42c0e74a1dc334
doan02012004 <doana1k24@gmail.com>
2024-06-12 10:37:38 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
